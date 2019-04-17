3792e08ce8e1bbf4ba5db91fd731e299356da0ab
Senyo <simpsonsenyo@gmail.com>
2019-04-17 20:28:44 +0200

    uses new var name instead of var name input

--- registration/warp/sample.py
+++ registration/warp/sample.py
@@ -1,12 +1,12 @@
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-def grid_sample(input, grid, canvas = None):
-    output = F.grid_sample(input, grid)
+def grid_sample(x, grid, canvas=None):
+    output = F.grid_sample(x, grid)
     if canvas is None:
         return output
     else:
-        input_mask = Variable(input.data.new(input.size()).fill_(1))
-        output_mask = F.grid_sample(input_mask, grid)
+        x_mask = Variable(x.data.new(x.size()).fill_(1))
+        output_mask = F.grid_sample(x_mask, grid)
         padded_output = output * output_mask + canvas * (1 - output_mask)
         return padded_output
