3ed79212f26a8531d79fe920bd9335d018e58e95
Senyo <simpsonsenyo@gmail.com>
2019-04-17 20:07:44 +0200

    mnist dataset

---
+++ registration/datasets.py
@@ -0,0 +1,43 @@
+import torch
+from torch.utils.data import Dataset
+import random
+import numpy as np
+import pandas as pd
+
+
+class MNIST(Dataset):
+    def __init__(self, path, size):
+        self.mnist, self.mnist_pairs = self.build_dataset(path, size)
+
+    def __getitem__(self, idx):
+        pair = self.mnist_pairs[idx]
+        return self.convert_pair_to_image(pair)
+
+    def __len__(self):
+        return len(self.mnist)
+
+    def build_dataset(self, path, size):
+        """ Builds a dataset on initialization, random pairing done each time """
+        ds = pd.read_csv(path, index_col=False)
+        grouped_ds = ds.groupby(['label'])
+        groups = grouped_ds.groups
+
+        generated_ds = []
+        for _ in range(size):
+            label = random.randint(0, 9)
+            pair = list(np.random.choice(groups[label], 2))
+            generated_ds.append(pair)
+
+        return ds, generated_ds
+
+    def convert_pair_to_image(self, pair):
+        image1_idx, image2_idx = pair
+        image1 = self.mnist.iloc[image1_idx][1:]
+        image2 = self.mnist.iloc[image2_idx][1:]
+        image1 = np.array(image1).reshape((28,28)).astype(np.uint8)
+        image2 = np.array(image2).reshape((28,28)).astype(np.uint8)
+        return (image1, image2)
+
+    def save_dataset(self):
+        # write to npy file
+        pass
