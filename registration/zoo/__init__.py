239e21356f404690afdbdc5ec213a01195b03adb
Senyo <simpsonsenyo@gmail.com>
2019-04-20 23:11:11 +0200

    residual block added to zoo

--- registration/zoo/__init__.py
+++ registration/zoo/__init__.py
@@ -1,3 +1,4 @@
 from .warpnet import WarpNet
 from .tps_stn import SpatialTransformer as ThinPlateTransformer
 from .affine_stn import SpatialTransformer as AffineTransformer
+from .residual import ResidualUnit
