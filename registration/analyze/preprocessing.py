af94152345bf8122cdd7f8a5b2a05ab7ec59cfd2
Senyo <simpsonsenyo@gmail.com>
2019-04-24 23:24:33 +0200

    preprocessing implementation complete

--- registration/analyze/preprocessing.py
+++ registration/analyze/preprocessing.py
@@ -8,9 +8,13 @@ import numpy as np
 from glob import glob
 from registration.analyze import imagetools
 
+DIM_RANGE = {
+    0: (4, 211), 
+    1: (40, 231), 
+    2: (70, 191)}
 
 if __name__ == "__main__":
-    logger = logging.getLogger('dir')
+    logger = logging.getLogger('preprocessing')
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -45,8 +49,10 @@ if __name__ == "__main__":
     with h5py.File(out, 'w') as f:
         f.create_dataset(
             name='Brain MRI Dataset',
-            shape=(148200, 2, 256, 256),
-            dtype=np.float64
+            shape=(72580, 2, 256, 256),
+            dtype=np.float32,
+            compression='gzip',
+            chunks=(32,2,256,256)
         )
         pos = 0
         for idx, image1_path in enumerate(image_paths):
@@ -54,18 +60,25 @@ if __name__ == "__main__":
             image1 = imagetools.get_image(image1_path, image_shape)
             image1 = np.pad(image1,((0,0), (2,2), (0,0)), mode='constant', constant_values=0)
             image1 = imagetools.resize_image(image1, (256,256,256))
-            for image2_path in image_paths[idx:]:
+            pair_image_paths = filter(lambda x: x != image1_path, image_paths)
+            for image2_path in pair_image_paths:
                 image2 = imagetools.get_image(image2_path, image_shape)
-                image2 = np.pad(image1,((0,0), (2,2), (0,0)), mode='constant', constant_values=0)
+                image2 = np.pad(image2,((0,0), (2,2), (0,0)), mode='constant', constant_values=0)
                 image2 = imagetools.resize_image(image2, (256,256,256))
-                for dim in (0, 1, 2):
-                    for i in range(60, 190): # chosen so that all dimensions have useful images
-                        image_pair = np.array([
-                                        imagetools.slice_image(image1, dim=dim, idx=i),
-                                        imagetools.slice_image(image2, dim=dim, idx=i)
-                                    ])
-                        assert image_pair.shape == (2, 256, 256), \
-                            'Image pair shape is incorrect. Should be (2, 256, 256)'
-                        f['Brain MRI Dataset'][i, :, :, :] = image_pair
-                        pos += 1
-        
+                # using only 1 dim to simplify the learning problem initially 
+                dim = 1
+                start, end = DIM_RANGE[dim]
+                for i in range(start, end):
+                    image_pair = np.array([
+                                    imagetools.slice_image(image1, dim=dim, idx=i),
+                                    imagetools.slice_image(image2, dim=dim, idx=i)
+                                ])
+                    assert image_pair.shape == (2, 256, 256), \
+                        'Image pair shape is incorrect. Should be (2, 256, 256)'
+                    f['Brain MRI Dataset'][pos, ...] = image_pair
+                    pos += 1
+
+
+        logger.info('Shuffling Dataset')
+        random.shuffle(f['Brain MRI Dataset'])
+    logger.info('Dataset Created')
