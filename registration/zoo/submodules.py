12698c8d12516f87e3329b5410acff79b3a3bbb5
Senyo <simpsonsenyo@gmail.com>
2019-04-25 15:56:49 +0200

    added network sub modules

--- registration/zoo/submodules.py
+++ registration/zoo/submodules.py
@@ -1,6 +1,25 @@
 import torch
 import torch.nn as nn
 
+def conv(batchNorm, in_channels, out_channels, kernel_size=3, stride=1):
+    if batchNorm:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
+            nn.ReLU(inplace=True)
+        )
+
+def deconv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=True),
+        nn.ReLU(inplace=True)
+    )
+
 
 class ResidualUnit(nn.Module):
     def __init__(self, params, bn_features, activation, shortcut=None):
