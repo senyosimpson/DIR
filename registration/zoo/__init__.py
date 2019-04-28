3369962744152db324e69b834450273ec14d0531
Senyo <simpsonsenyo@gmail.com>
2019-04-28 08:48:19 +0200

    updated imports

--- registration/zoo/__init__.py
+++ registration/zoo/__init__.py
@@ -1,4 +1,3 @@
 from .warpnet import WarpNet
 from .tps_stn import SpatialTransformer as ThinPlateTransformer
 from .affine_stn import SpatialTransformer as AffineTransformer
-from .residual import ResidualUnit
