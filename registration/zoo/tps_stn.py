6726ff5343feede2ce71704dfaf80510375b82c6
Senyo <simpsonsenyo@gmail.com>
2019-04-28 08:46:27 +0200

    works with our code

--- registration/zoo/tps_stn.py
+++ registration/zoo/tps_stn.py
@@ -12,16 +12,16 @@ from registration.warp import TPSGridGen
 class CNN(nn.Module):
     def __init__(self, num_output):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, padding=2)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_output)
+        self.fc1 = nn.Linear(2048, 256)
+        self.fc2 = nn.Linear(256, num_output)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = x.view(-1, 2048)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
