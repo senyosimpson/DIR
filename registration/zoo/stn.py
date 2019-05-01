5e5c35831c295c22fd59cc5d8c4f79e3875c017f
Senyo <simpsonsenyo@gmail.com>
2019-05-01 19:41:44 +0200

    tps stn with different implementation

---
+++ registration/zoo/stn.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+from registration.warp import thinplate
+
+
+class SpatialTransformer(nn.Module):
+    def __init__(self, outshape=(256, 256), ctrlshape=(12, 12)):
+        super().__init__()
+        self.nctrl = np.prod(ctrlshape)
+        self.outshape = outshape
+        self.nparam = self.nctrl + 2
+        ctrl = thinplate.uniform_grid(ctrlshape)
+        self.register_buffer('ctrl', ctrl.view(-1,2))
+
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, padding=2)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(2048, 256)
+        self.fc2 = nn.Linear(256, self.nparam*2)
+
+        self.fc2.weight.data.normal_(0, 1e-3)
+        self.fc2.bias.data.zero_()
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=6, padding=3))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), kernel_size=6, padding=3))
+        x = x.view(32, -1)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.tanh(self.fc2(x))
+
+        theta = x.view(-1, self.nparam, 2)
+        grid = thinplate.tps_grid(theta, self.ctrl, (x.shape[0], ) + self.outshape)
+        transformed_x = F.grid_sample(x, grid)
+        return transformed_x
--- registration/zoo/submodules.py
+++ registration/zoo/submodules.py
@@ -20,28 +20,5 @@ def deconv(in_channels, out_channels):
         nn.ReLU(inplace=True)
     )
 
-
-class ResidualUnit(nn.Module):
-    def __init__(self, params, bn_features, activation, shortcut=None):
-        super().__init__()
-        self.conv1_params, self.conv2_params = params
-        self.bn1_features, self.bn2_features = bn_features
-        self.conv1 = nn.Conv2d(**self.conv1_params)
-        self.conv2 = nn.Conv2d(**self.conv2_params)
-        self.bn1 = nn.BatchNorm2d(self.bn1_features)
-        self.bn2 = nn.BatchNorm2d(self.bn2_features)
-        self.activation = activation()
-        self.shortcut = shortcut
-
-    def forward(self, x):
-        input_tensor = x
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.activation(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        if self.shortcut:
-            input_tensor = self.shortcut(input_tensor)
-        x = torch.add(x, input_tensor)
-        x = self.activation(x)
-        return x
+def predict_flow(in_planes):
+    return nn.Conv2d(in_planes,2,kernel_size=3,stride=1,padding=1,bias=True)
