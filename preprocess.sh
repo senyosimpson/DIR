ec9cc6f010106dee69569ad9baa8b3c262136a96
Senyo <simpsonsenyo@gmail.com>
2019-05-02 17:40:09 +0200

    upated output file name

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 python3 -m registration.analyze.preprocessing \
 --root registration/data/MRI \
---out registration/data/dataset.h5 \
+--out registration/data/mri-dataset.h5 \
 --header registration/data/MRI/native.mri.hdr
 
