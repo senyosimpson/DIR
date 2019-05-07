4e0396f5898815bfd3b2c6d79f18efc5e6c2c609
Senyo <simpsonsenyo@gmail.com>
2019-05-07 19:04:04 +0200

    changed path

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,4 @@
-python3 -m registration.analyze.preprocessing \
+python3 -m registration.preprocessing \
 --root registration/data/MRI \
 --out registration/data/mri-dataset.h5 \
 --header registration/data/MRI/native.mri.hdr
