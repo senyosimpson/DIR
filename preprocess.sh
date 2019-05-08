ecfef27c8beccfa2fa645b29fa7263ef613a7f8d
Senyo <simpsonsenyo@gmail.com>
2019-05-08 10:30:41 +0200

    name change

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,4 @@
-python3 -m registration.preprocessing \
+python3 -m registration.preprocess \
 --root registration/data/MRI \
 --out registration/data/mri-dataset.h5 \
 --header registration/data/MRI/native.mri.hdr
